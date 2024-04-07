9053979ada05d6973d1cecc969ca458d684a9700
Brenda Mukaya <brendamukaya@gmail.com>
2024-04-07 18:24:35 +0300

    modified to be git pushed

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+const writeCards =(namesArr, eventName)=> {
+ const thankYouArr = [];
+ for (let i = 0; i < namesArr.length;i++){ 
+    thankYouArr.push(
+        "Thank you, ${namesArr[i]} for the wonderful ${eventname} gift!");
+    }
+
+
+    return thankYouArr;
+
+ }
+
+const countDown = (number) => {
+    while (number >=0) {
+        console.log (number);
+        number--;
+
+    }
+};
+
